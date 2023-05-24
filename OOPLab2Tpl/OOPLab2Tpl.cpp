522c2f5cf4355195b538ceb2e9a5afed87d37819
Oleksandr Kalita <olexadr.kalita@gmail.com>
2023-05-24 15:43:08 +0200

    new one
    adding namespase std and main function

--- OOPLab2Tpl/OOPLab2Tpl.cpp
+++ OOPLab2Tpl/OOPLab2Tpl.cpp
@@ -1,10 +1,11 @@
 ﻿#include <iostream>
 #include <string>
 #include <bitset>
+using namespace std;
 
 // Функція для доповнення тексту пробілами до 128 символів
-std::string padText(const std::string& text) {
-    std::string paddedText = text;
+string padText(const string& text) {
+    string paddedText = text;
     if (paddedText.length() < 128) {
         paddedText.append(128 - paddedText.length(), ' ');
     }
@@ -12,8 +13,8 @@ std::string padText(const std::string& text) {
 }
 
 // Функція для шифрування символу
-std::bitset<16> encryptCharacter(char c, int position) {
-    std::bitset<16> encrypted;
+bitset<16> encryptCharacter(char c, int position) {
+    bitset<16> encrypted;
     unsigned char asciiCode = static_cast<unsigned char>(c);
 
     encrypted |= (asciiCode >> 4);        // Старша частина ASCII-коду
@@ -25,8 +26,8 @@ std::bitset<16> encryptCharacter(char c, int position) {
 }
 
 // Функція для шифрування тексту
-std::bitset<16>* encryptText(const std::string& text) {
-    std::bitset<16>* encryptedText = new std::bitset<16>[128];
+bitset<16>* encryptText(const string& text) {
+    bitset<16>* encryptedText = new bitset<16>[128];
 
     for (int i = 0; i < text.length(); i++) {
         char c = text[i];
@@ -34,4 +35,22 @@ std::bitset<16>* encryptText(const std::string& text) {
     }
 
     return encryptedText;
+}
+
+int main() {
+    string inputText;
+    cout << "Введіть текст до 128 символів: ";
+    getline(cin, inputText);
+
+    string paddedText = padText(inputText);
+    bitset<16>* encryptedText = encryptText(paddedText);
+
+    cout << "Шифрований текст: " << endl;
+    for (int i = 0; i < paddedText.length(); i++) {
+        cout << "Символ '" << paddedText[i] << "': " << encryptedText[i] << endl;
+    }
+
+    delete[] encryptedText;
+
+    return 0;
 }
