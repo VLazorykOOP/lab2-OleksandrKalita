c64e956541c007d4ff2642bcdf96eb5c4aadd5e2
Oleksandr Kalita <olexadr.kalita@gmail.com>
2023-05-24 15:20:21 +0200

    first commit

--- OOPLab2Tpl/OOPLab2Tpl.cpp
+++ OOPLab2Tpl/OOPLab2Tpl.cpp
@@ -1,1 +1,12 @@
-﻿
+﻿#include <iostream>
+#include <string>
+#include <bitset>
+
+// Функція для доповнення тексту пробілами до 128 символів
+std::string padText(const std::string& text) {
+    std::string paddedText = text;
+    if (paddedText.length() < 128) {
+        paddedText.append(128 - paddedText.length(), ' ');
+    }
+    return paddedText;
+}
